ed7df5e93a49e488362355006ceb1b9cc98cd0d8
Szilágyi Zalán <zalanka12345678@gmail.com>
2022-03-08 15:09:54 +0100

    grafikus resz keszen

--- 15drill.cpp
+++ 15drill.cpp
@@ -1,8 +1,17 @@
 #include "Simple_window.h"
 #include "Graph.h"
+using namespace std;
+struct Person
+{
+	string name;
+	int age;
+
+
+};
 double one(double x) {return 1;}
 double slope(double x) { return x/2; }
 double square(double x) { return x*x; }
+double sloping_cos(double x) { return cos(x/2); }
 int main()
 {
 	using namespace Graph_lib;
@@ -25,11 +34,18 @@ int main()
 	Function sl (one, slmin, slmax, crossp, sllength, x_scale, y_scale);
 	Function slp(slope, slmin, slmax, crossp, sllength, x_scale, y_scale);
 	Function sqre(square, slmin, slmax, crossp, sllength, x_scale, y_scale);
+	Function cosine(cos,slmin,slmax,crossp,sllength,x_scale,y_scale);
+	Function slp_cos(sloping_cos,slmin,slmax,crossp,sllength,x_scale,y_scale);
+	Text label (Point(x_cross-200,y_cross+80),"x/2");
+	cosine.set_color(Color::cyan);
+	win.attach(label);
 	win.attach(x);
 	win.attach(y);
 	win.attach(sl);
 	win.attach(slp);
 	win.attach(sqre);
+	win.attach(cosine);
+	win.attach(slp_cos);
 	win.wait_for_button();
 
 }
