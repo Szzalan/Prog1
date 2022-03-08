0d1c88c76d09d4ef64b4e937fabdb8e2b6fdd34e
Szilágyi Zalán <zalanka12345678@gmail.com>
2022-03-08 15:10:31 +0100

    klasszok

---
+++ 15drillclass.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+struct Person
+{
+	string name;
+	int age;
+
+
+};
+int main()
+{
+	Person obj1;
+	obj1.name = "Goofy";
+	obj1.age = 63;
+	cout << obj1.name << endl;	
+
+
+
+
+
+
+}
