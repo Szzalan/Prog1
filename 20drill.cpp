2c6e6836e95e8925d9b2449a7c0f6d87d9dfedbe
Szilágyi Zalán <zalanka12345678@gmail.com>
2022-05-26 15:54:59 +0200

    with comment

--- 20drill.cpp
+++ 20drill.cpp
@@ -1,12 +1,15 @@
 #include "std_lib_facilities.h"
-template<typename Iter1, typename Iter2>
+template<typename Iter1, typename Iter2> //iterátor copy
 Iter2 copyer(Iter1 f1, Iter1 e1, Iter2 f2)
 {
-	while( f1 != e1)
+	while( f1 != e1) //amíg az első konténer tart
 	{
-		*f2 = *f1;
+		*f2 = *f1; //értékadás
+
+		f2++; //léptetés
+
 		f1++;
-		f2++;	
+			
 	}
 
  return f2;
@@ -17,7 +20,7 @@ int main(){
 	array<int,length> ar;
 	vector<int> vec;
 	list <int> ls;
-		for (int i = 0; i < length; ++i)
+		for (int i = 0; i < length; ++i) //értékadás
 		{
 			ar[i] = i;
 		}
@@ -33,7 +36,7 @@ int main(){
 		}
 			for (array<int, length>::iterator iter = ar.begin(); iter != ar.end(); iter++)
 			{
-				*iter += 2;
+				*iter += 2; //értéket eléri majd hozzáad 2-t
 			}
 			for (vector<int>::iterator iter = vec.begin(); iter != vec.end(); iter++)
 			{
@@ -49,13 +52,21 @@ int main(){
 	vector<int> vec2= vec;
 	list<int> ls2= ls;
 	//copipasta
-	copyer(ar2.begin(),ar2.end(),vec2.begin());
+	copyer(ar2.begin(),ar2.end(),vec2.begin()); //copy használat
 	copyer(ls2.begin(),ls2.end(),ar2.begin());
+	for (array<int, length>::iterator iter = ar2.begin(); iter != ar2.end(); iter++)
+			{
+				cout << *iter << endl; //kiíratás
+			}
+	for (vector<int>::iterator iter = vec2.begin(); iter != vec2.end(); iter++)
+			{
+				cout << *iter << endl;
+			}
 	//find
-	vector<int>::iterator fi = find(vec2.begin(),vec2.end(),3);
+	vector<int>::iterator fi = find(vec2.begin(),vec2.end(),3); //megkapja a helyzetét
 	if (fi != vec2.end())
 	{
-		cout << "The searched position is: " << distance(vec2.begin(),fi) << endl;
+		cout << "The searched position is: " << distance(vec2.begin(),fi) << endl; //így íratjuk ki
 	}
 	else
 		cout << "The searched number is not found" << endl;
